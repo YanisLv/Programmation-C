bb91c86250aaec5690acab4d3579bb11c3853afe
YanisLv <lezhayaya213@gmail.com>
2025-10-22 17:23:07 +0200

    avancement td4

--- td4/exo5.c
+++ td4/exo5.c
@@ -1,4 +1,62 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void damier(int t, int c) {
+    int i, j;
+    for (i = 0; i < t * c; i++) { 
+        for (j = 0; j < t * c; j++) { 
+            // On alterne les motifs selon la "parité" du bloc courant
+            if ( ((i / c) + (j / c)) % 2 == 0 )
+                printf("X");
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int t = 4, c = 2;
+    damier(t, c);
+    return 0;
+}
+
+
+
+/*
+void damier(int t, int c){
+    for(int j = 0; j< t; j++){
+        if(j%t == 0){
+            for(int k = 0; k < c; k++){
+                for(int i = 0; i < t; i++){
+                    if((i%c) == 0){
+                        printf("XX");
+                    }
+                    else{
+                        printf("..");
+                    }
+                }
+                printf("\n");
+            }
+        }
+        else{
+            for(int k = 0; k < c; k++){
+                for(int i = 0; i < t; i++){
+                    if((i%c) == 0){
+                        printf("..");
+                    }
+                    else{
+                        printf("XX");
+                    }
+                }
+                printf("\n");
+            }
+        }
+        
+   }
+
+}
+*/
+/*
 void damier(int t, int c){
     for(int k = 0; k<c;k++){
         for(int i = 0; i < c; i++){
@@ -17,6 +75,7 @@ void damier(int t, int c){
         }
     }
 }
+*/
 int main()
 {
     damier(4,2);
